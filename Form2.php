0918498b1fe8df8b4b8001538e1490c904114a22
MATI UL REHMAN <74356765+Matiz009@users.noreply.github.com>
2021-02-01 16:36:56 +0500

    some changes has been made and php is added in form element

---
+++ Form2.php
@@ -0,0 +1,32 @@
+<?php
+
+$Name= $_POST['Name'];
+$fName=$_POST['fName'];
+$Date=$_POST['Date'];
+$Address=$_POST['Address'];
+$radio_value=$_POST['radio'];
+$radio1_value=$_POST['age'];
+$radio2_value=$_POST['class'];
+$checkbox_value=$_POST['grade'];
+$admission=$_POST['admission'];
+$answer=$_POST['ans'];
+
+
+echo "Full name is : "." ".$Name;
+echo "<br>Father name is : "." ".$fName;
+echo "<br>Date of birth is : "." ".$Date;
+echo "<br>Address is : "." ".$Address;
+echo "<br>Gender is : "." ".$radio_value;
+echo "<br>Age is : "." ".$radio1_value;
+echo "<br>Class is : "." ".$radio2_value;
+echo "<br>Grade is :"." ".$checkbox_value;
+echo "<br>Applied in class"." ".$admission;
+echo"<br>Answer is :"." ".$answer;
+
+
+
+
+
+
+
+
